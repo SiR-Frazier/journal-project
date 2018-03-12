9915b78f2e0bdeb3ecf5fb5f28b7bbb3931a1734
Bryce Frazier and Kimberly Huynh <kimberly@>
2018-03-12 15:06:50 -0700

    add lint and web development server to project

--- src/journal.js
+++ src/journal.js
@@ -5,16 +5,16 @@ export function Entry(title, log) {
 
 Entry.prototype.vowelCounter = function () {
   var inputtedLog = this.entryLog.match(/[aeiou]/gi).length;
-}
+};
 
 Entry.prototype.consonantCounter = function() {
   var inputtedLog = this.entryLog.match(/[bcdfghjklmnpqrstvxzwy]/gi).length;
-}
+};
 
 Entry.prototype.wordCounter = function() {
   var inputtedLog = this.entryLog.split(' ').length;
-}
+};
 
 Entry.prototype.getTeaser = function() {
-  var inputtedLog = this.entryLog.split(/\s+/).slice(0,8)
-}
+  var inputtedLog = this.entryLog.split(/\s+/).slice(0,8);
+};

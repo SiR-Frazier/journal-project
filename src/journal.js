bcfdd1f88cdfeeef91419481c430e185117800fc
Bryce Frazier and Kimberly Huynh <kimberly@>
2018-03-12 13:16:47 -0700

    add logic to journal.js file

--- src/journal.js
+++ src/journal.js
@@ -1,10 +1,28 @@
-function Entry(title, description) {
+function Entry(title, log) {
   this.entryTitle = title;
-  this.entryDescription = description;
+  this.entryLog = log;
 }
 
 // var log1 = new Entry("test sentence");
+function entryMaker() {
+  var title = $("#title").val();
+  var log = $("#log").val();
+  var newEntry = new Entry(title,log);
+  return newEntry;
+}
+
+Entry.prototype.vowelCounter = function () {
+  var inputtedLog = this.entryLog.match(/[aeiou]/gi).length;
+};
+
+Entry.prototype.consonantCounter = function() {
+  var inputtedLog = this.entryLog.match(/[bcdfghjklmnpqrstvxzwy]/gi).length;
+}
 
 Entry.prototype.wordCounter = function() {
-  var log = 
+  var inputtedLog = this.entryLog.split(' ').length;
+}
+
+Entry.prototype.getTeaser = function() {
+  var inputtedLog = this.entryLog.split(/\s+/).slice(0,8)
 }

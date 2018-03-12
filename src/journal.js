b630823a244ed0f9b85f0fac8c1ff336918573e6
Bryce Frazier and Kimberly Huynh <bryce@>
2018-03-12 11:16:26 -0700

    initialize files and start layout/js

---
+++ src/journal.js
@@ -0,0 +1,10 @@
+function Entry(title, description) {
+  this.entryTitle = title;
+  this.entryDescription = description;
+}
+
+// var log1 = new Entry("test sentence");
+
+Entry.prototype.wordCounter = function() {
+  var log = 
+}
